36efdcf73941686b39da46b81f05ba7afd1a4438
Ales Janko <alesjanko.info@gmail.com>
2020-10-24 20:15:53 +0200

    static var inside function

--- vaja1/main.c
+++ vaja1/main.c
@@ -6,6 +6,14 @@
 int my_external_var = 5;
 extern int my_external_var2;
 
+int static_count_fun()
+{
+    static int count = 0;
+
+    count++;
+    return count;
+}
+
 int main()
 {
     printf("%d\n", getnumber());
@@ -13,6 +21,9 @@ int main()
     printf("%i\n", return_extern_var());
 
      printf("%i\n", my_external_var2);
+
+     printf("%d\n", static_count_fun());
+       printf("%d\n", static_count_fun());
 
     return 0;
 }
